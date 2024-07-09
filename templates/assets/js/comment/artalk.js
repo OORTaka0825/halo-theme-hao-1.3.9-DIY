3899934db7d465ebed65d2d637d51f7e62f6e652
OORTaka0825 <oortaka0825@gmail.com>
2024-07-10 00:16:03 +0800

    提交

--- templates/assets/js/comment/artalk.js
+++ templates/assets/js/comment/artalk.js
@@ -2,44 +2,23 @@
     if (!document.getElementById('post-comment')) return
     const initArtalk = () => {
 
-        window.artalkItem = Artalk.init(Object.assign({
+        window.artalkItem = new Artalk(Object.assign({
             el: '#artalk-wrap',
             server: GLOBAL_CONFIG.source.artalk.artalkUrl,
             site: GLOBAL_CONFIG.source.artalk.siteName,
-            pageKey: location.pathname.replace(/\/page\/\d$/, ""),
+            pageKey: location.pathname,
             darkMode: false,
             countEl: '#ArtalkCount'
         }, null))
 
-        function versionOld(ctx){
-            // 旧版本兼容性补丁
-            ctx.getCommentList().forEach(comment => {
-                const $content = comment.getRender().$content
-                btf.loadLightbox($content.querySelectorAll('img:not([atk-emoticon])'))
-            })
-        }
-
-        function version_2_7_3_WithUpper(ctx){
-            // 2.7.3 版本及以后版本支持
-            ctx.get('list').getCommentNodes().forEach(comment => {
-                const $content = comment .getRender().$content
-                btf.loadLightbox($content.querySelectorAll('img:not([atk-emoticon])'))
-            })
-
-        }
-
-        function versionCheck(ctx){
-            if(ctx.getCommentList != undefined){
-                // Artalk 版本小于于 2.7.3
-                versionOld(ctx);
-            }else{
-                version_2_7_3_WithUpper(ctx);
-            }
-        }
-
         if (GLOBAL_CONFIG.lightbox === 'null') return
-        window.artalkItem.on('list-loaded', () => {
-            versionCheck(window.artalkItem.ctx);
+        window.artalkItem.use(ctx => {
+            ctx.on('list-loaded', () => {
+                ctx.getCommentList().forEach(comment => {
+                    const $content = comment.getRender().$content
+                    btf.loadLightbox($content.querySelectorAll('img:not([atk-emoticon])'))
+                })
+            })
         })
     }
 
