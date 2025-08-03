18eb754403aa176e6fa4d041567a1d6d8e45374c
Taka <140028069+OORTaka0825@users.noreply.github.com>
2025-08-04 03:28:33 +0800

    更新 welcome.js

--- templates/assets/libs/welcome/welcome.js
+++ templates/assets/libs/welcome/welcome.js
@@ -1,10 +1,11 @@
+
 // 访客欢迎信息模块（NSMAO）
 let ipLocation;
 
 // 计算两点间距离
 function getDistance(e1, n1, e2, n2) {
     const R = 6371;
-    const { sin， cos, asin, PI， hypot } = Math;
+    const { sin, cos, asin, PI, hypot } = Math;
     let getPoint = (e, n) => {
         e *= PI / 180;
         n *= PI / 180;
@@ -12,7 +13,7 @@ function getDistance(e1, n1, e2, n2) {
     };
     let a = getPoint(e1, n1);
     let b = getPoint(e2, n2);
-    let c = hypot(a。x - b。x， a。y - b。y， a。z - b。z);
+    let c = hypot(a.x - b.x, a.y - b.y, a.z - b.z);
     let r = asin(c / 2) * 2 * R;
     return Math.round(r);
 }
@@ -27,59 +28,51 @@ function fetchIpLocation() {
         },
         dataType: 'json',
         success: function (res) {
-            console。log("API请求成功:"， res);  // 确保API请求成功
-            if (res.code !== 200) {
-                console。error("IP定位失败:"， res);
-                return;
-            }
+            if (res.code !== 200) return;
             ipLocation = {
                 ip: res.data.ip,
                 location: {
                     lat: res.data.lat,
                     lng: res.data.lng
-                }，
+                },
                 ad_info: {
                     nation: res.data.country,
                     province: res.data.province,
-                    city: res.data。city，
+                    city: res.data.city,
                     district: res.data.district
                 }
             };
             showWelcome();
-        },
-        error: function (err) {
-            console。error("API请求失败:"， err);  // 如果请求失败
         }
     });
 }
 
 // 展示欢迎语
 function showWelcome() {
-    if (!ipLocation) {
-        console。error("ipLocation为空");
-        return;
-    }
-
+    if (!ipLocation) return;
     const myLng = GLOBAL_CONFIG.source.welcome.lng * 1;
-    const myLat = GLOBAL_CONFIG。source。welcome。lat * 1;
+    const myLat = GLOBAL_CONFIG.source.welcome.lat * 1;
     let dist = getDistance(myLng, myLat, ipLocation.location.lng, ipLocation.location.lat);
-    let city = ipLocation。ad_info。city;
-    let province = ipLocation.ad_info.province;
-    let district = ipLocation。ad_info.district;
+    let pos = ipLocation.ad_info.nation;
     let ip = ipLocation.ip;
     let desc = '带我去你的城市逛逛吧！';
 
-    // 更正位置展示
-    let pos = `${province} ${city} ${district}`;
-
-    if (city === "北京市") {
-        desc = "北——京——欢迎你~";
-    } else if (city === "广州市") {
-        desc = "看小蛮腰，喝早茶了嘛~";
-    } else if (city === "深圳市") {
-        desc = "今天你逛商场了嘛~";
-    } else {
-        desc = "来自 " + city + " 的小伙伴你好呀~";
+    if (pos === "中国") {
+        pos = ipLocation.ad_info.province + " " + ipLocation.ad_info.city;
+        let city = ipLocation.ad_info.city;
+        switch (city) {
+            case "北京市":
+                desc = "北——京——欢迎你~";
+                break;
+            case "广州市":
+                desc = "看小蛮腰，喝早茶了嘛~";
+                break;
+            case "深圳市":
+                desc = "今天你逛商场了嘛~";
+                break;
+            default:
+                desc = "来自 " + city + " 的小伙伴你好呀~";
+        }
     }
 
     let date = new Date();
@@ -93,21 +86,10 @@ function showWelcome() {
 
     if (ip.includes(":")) ip = "好复杂，咱看不懂~(ipv6)";
 
-    const content = `
-        <b><span style="color: var(--kouseki-ip-color);font-size: var(--kouseki-gl-size)">${pos}</span></b><br>
-        ${desc}🍂<br>
-        当前位置距博主约 <b><span style="color: var(--kouseki-ip-color)">${dist}</span></b> 公里！<br>
-        您的IP地址为：<b><span>${ip}</span></b><br>
-        ${greet} <br>
-    `;
+    const content = `欢迎来自 <b><span style="color: var(--kouseki-ip-color);font-size: var(--kouseki-gl-size)">${pos}</span></b> 的小友💖<br>${desc}🍂<br>当前位置距博主约 <b><span style="color: var(--kouseki-ip-color)">${dist}</span></b> 公里！<br>您的IP地址为：<b><span>${ip}</span></b><br>${greet} <br>`;
 
     try {
-        const welcomeElement = document.getElementById("welcome-info");
-        if (welcomeElement) {
-            welcomeElement.innerHTML = content;
-        } else {
-            console.error("未找到元素#welcome-info");
-        }
+        document.getElementById("welcome-info").innerHTML = content;
     } catch (err) {
         console.log("欢迎模块插入失败:", err);
     }
