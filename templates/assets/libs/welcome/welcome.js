ffb7b58dd3db5c04f0cea07e5f6fc5f0ae948925
Taka <140028069+OORTaka0825@users.noreply.github.com>
2025-08-04 03:14:42 +0800

    更新 welcome.js

--- templates/assets/libs/welcome/welcome.js
+++ templates/assets/libs/welcome/welcome.js
@@ -1,3 +1,4 @@
+
 // 访客欢迎信息模块（NSMAO）
 let ipLocation;
 
@@ -24,16 +25,16 @@ function fetchIpLocation() {
         url: 'https://api.nsmao.net/api/ip/query',
         data: {
             key: GLOBAL_CONFIG.source.welcome.key
-        }，
+        },
         dataType: 'json',
         success: function (res) {
-            if (res。code !== 200) return;
+            if (res.code !== 200) return;
             ipLocation = {
-                ip: res.data.ip，
+                ip: res.data.ip,
                 location: {
                     lat: res.data.lat,
                     lng: res.data.lng
-                }，
+                },
                 ad_info: {
                     nation: res.data.country,
                     province: res.data.province,
@@ -49,75 +50,43 @@ function fetchIpLocation() {
 // 展示欢迎语
 function showWelcome() {
     if (!ipLocation) return;
-
     const myLng = GLOBAL_CONFIG.source.welcome.lng * 1;
     const myLat = GLOBAL_CONFIG.source.welcome.lat * 1;
     let dist = getDistance(myLng, myLat, ipLocation.location.lng, ipLocation.location.lat);
     let pos = ipLocation.ad_info.nation;
     let ip = ipLocation.ip;
-    let posdesc = '带我去你的城市逛逛吧！';
+    let desc = '带我去你的城市逛逛吧！';
 
-    // 根据国家、省份、城市自定义欢迎语
-    switch (pos) {
-        case "日本":
-            posdesc = "よろしく，一起去看樱花吗";
-            break;
-        case "美国":
-            posdesc = "Let us live in peace!";
-            break;
-        case "英国":
-            posdesc = "想同你一起夜乘伦敦眼";
-            break;
-        case "俄罗斯":
-            posdesc = "干了这瓶伏特加！";
-            break;
-        case "法国":
-            posdesc = "C'est La Vie";
-            break;
-        case "德国":
-            posdesc = "Die Zeit verging im Fluge.";
-            break;
-        case "澳大利亚":
-            posdesc = "一起去大堡礁吧！";
-            break;
-        case "加拿大":
-            posdesc = "拾起一片枫叶赠予你";
-            break;
-        case "中国":
-            pos = ipLocation.ad_info.province + " " + ipLocation.ad_info.city + " " + ipLocation.ad_info.district;
-            let city = ipLocation.ad_info.city;
-            switch (city) {
-                case "北京市":
-                    posdesc = "北——京——欢迎你~~~";
-                    break;
-                case "广州市":
-                    posdesc = "看小蛮腰，喝早茶了嘛~";
-                    break;
-                case "深圳市":
-                    posdesc = "今天你逛商场了嘛~";
-                    break;
-                default:
-                    posdesc = "来自 " + city + " 的小伙伴你好呀~";
-            }
-            break;
-        default:
-            posdesc = "带我去你的国家逛逛吧";
-            break;
+    if (pos === "中国") {
+        pos = ipLocation.ad_info.province + " " + ipLocation.ad_info.city;
+        let city = ipLocation.ad_info.city;
+        switch (city) {
+            case "北京市":
+                desc = "北——京——欢迎你~";
+                break;
+            case "广州市":
+                desc = "看小蛮腰，喝早茶了嘛~";
+                break;
+            case "深圳市":
+                desc = "今天你逛商场了嘛~";
+                break;
+            default:
+                desc = "来自 " + city + " 的小伙伴你好呀~";
+        }
     }
 
-    // 根据本地时间切换欢迎语
-    let timeChange;
     let date = new Date();
     let hour = date.getHours();
-    if (hour >= 5 && hour < 11) timeChange = "🌤️ 早上好，一日之计在于晨";
-    else if (hour < 13) timeChange = "☀️ 中午好，记得午休喔~";
-    else if (hour < 17) timeChange = "🕞 下午好，饮茶先啦！";
-    else if (hour < 19) timeChange = "🚶‍♂️ 即将下班，记得按时吃饭~";
-    else timeChange = "🌙 晚上好，夜生活嗨起来！";
+    let greet = "夜深了，早点休息~";
+    if (hour >= 5 && hour < 11) greet = "🌤️ 早上好，一日之计在于晨";
+    else if (hour < 13) greet = "☀️ 中午好，记得午休喔~";
+    else if (hour < 17) greet = "🕞 下午好，饮茶先啦！";
+    else if (hour < 19) greet = "🚶‍♂️ 即将下班，记得按时吃饭~";
+    else if (hour < 24) greet = "🌙 晚上好，夜生活嗨起来！";
 
     if (ip.includes(":")) ip = "好复杂，咱看不懂~(ipv6)";
 
-    const content = `欢迎来自 <b><span style="color: var(--kouseki-ip-color);font-size: var(--kouseki-gl-size)">${pos}</span></b> 的小友💖<br>${posdesc}🍂<br>当前位置距博主约 <b><span style="color: var(--kouseki-ip-color)">${dist}</span></b> 公里！<br>您的IP地址为：<b><span>${ip}</span></b><br>${timeChange} <br>`;
+    const content = `欢迎来自 <b><span style="color: var(--kouseki-ip-color);font-size: var(--kouseki-gl-size)">${pos}</span></b> 的小友💖<br>${desc}🍂<br>当前位置距博主约 <b><span style="color: var(--kouseki-ip-color)">${dist}</span></b> 公里！<br>您的IP地址为：<b><span>${ip}</span></b><br>${greet} <br>`;
 
     try {
         document.getElementById("welcome-info").innerHTML = content;
